1651fa25942bc62a187e65e62cda0fdadb18e4e9
Dmitry <dima.subbot@list.ru>
2020-02-13 00:46:45 +0300

    release v1

---
+++ task01-vowel-count.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+// Задача: реализовать метод CountVowels, который должен подсчитывать количество гласных символов в переданной строке.
+//   * Гласными считаются символы - 'a', 'e', 'i', 'o', 'u'.
+//   * Метод должен выбрасывать исключение ArgumentNullException в случае, если в метод передали null.
+//   * В решении разрешается использовать только конструкции языка. Использовать LINQ запрещено.
+
+namespace Task1
+{
+    public class Program
+    {
+        public static int CountVowels(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                throw new ArgumentNullException("Передано значение null в качестве параметра в метод CountVowels()!");
+            }
+            int counter = 0;
+            char[] arr = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            for(int i = 0; i < s.Length; ++i)
+            {
+                for(int j = 0; j < arr.Length; ++j)
+                {
+                    if(s[i] == arr[j])
+                    {
+                        ++counter;
+                        break;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Task is done when all test cases are correct:");
+
+            int testCaseNumber = 1;
+
+            TestReturnedValues(testCaseNumber++, "", 0);
+            TestReturnedValues(testCaseNumber++, " ", 0);
+            TestReturnedValues(testCaseNumber++, "a", 1);
+            TestReturnedValues(testCaseNumber++, "b", 0);
+            TestReturnedValues(testCaseNumber++, "ab", 1);
+            TestReturnedValues(testCaseNumber++, "ba", 1);
+            TestReturnedValues(testCaseNumber++, "aba", 2);
+            TestReturnedValues(testCaseNumber++, "bab", 1);
+            TestReturnedValues(testCaseNumber++, "aeiou", 5);
+            TestReturnedValues(testCaseNumber++, "bacedifoguh", 5);
+            TestReturnedValues(testCaseNumber++, "Lorem ipsum dolor sit amet", 9);
+            TestException<ArgumentNullException>(testCaseNumber++, null);
+        }
+
+        private static void TestReturnedValues(int testCaseNumber, string s, int expectedResult)
+        {
+            try
+            {
+                if (CountVowels(s) == expectedResult)
+                {
+                    Console.WriteLine(correctCaseTemplate, testCaseNumber);
+                }
+                else
+                {
+                    Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(correctCaseTemplate, testCaseNumber);
+            }
+        }
+
+        private static void TestException<T>(int testCaseNumber, string s) where T : Exception
+        {
+            try
+            {
+                CountVowels(s);
+                Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine(correctCaseTemplate, testCaseNumber);
+                correctTestCaseAmount++;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+            }
+        }
+
+        private static string correctCaseTemplate = "Case #{0} is correct.";
+        private static string incorrectCaseTemplate = "Case #{0} IS NOT CORRECT";
+        private static int correctTestCaseAmount = 0;
+    }
+}
---
+++ task05-prime-numbers.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Задача: реализовать метод GetPrimeNumbers, который должен возвращать массив простых чисел, находящихся в исследуемой последовательности чисел.
+//   * Массив должен содержать значения в возрастающем порядке.
+//   * Параметры start и end задают граничные значения исследуемой последовательности чисел (включительно).
+//   * Выбрасывать исключение ArgumentException в случае, если значения параметров не являются корректными.
+//   * В решении разрешается использовать только конструкции языка и стандартные структуры данных BCL. Использовать LINQ запрещено.
+
+namespace Task5
+{
+    public class Program
+    {
+        public static int[] GetPrimeNumbers(int start, int end)
+        {
+            if(start > end)
+            {
+                throw new ArgumentException("Значения параметров не являются корректными. Начальное граничное значения должно быть меньше или равно конечному");
+            }
+            if(start <= 1 || end <= 1) //ряд простых чисел начинаются с 2-ки (0 и 1 не являются простыми)
+            {
+                throw new ArgumentException("Значения параметров не являются корректными. Начальное и конечное граничные значения должны принадлежать натуриальному ряду чисел поэтому не могут рвняться нулю или быть отрицательными!");
+            }
+            List<int> list = new List<int>();
+            for(int i = start; i <= end; ++i)
+            {
+                if (IsPrime(i))
+                {
+                    list.Add(i);
+                }
+            }
+            return list.ToArray();
+        }
+
+        public static bool IsPrime(int value)
+        {
+            bool flag = false;
+            for(int i = 2; i < value; ++i)
+            {
+                if(value % i == 0)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (value > 1 && flag == false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Task is done when all test cases are correct:");
+
+            int testCaseNumber = 1;
+
+            TestReturnValues(testCaseNumber++, 2, 2, new int[] { 2 });
+            TestReturnValues(testCaseNumber++, 2, 3, new int[] { 2, 3 });
+            TestReturnValues(testCaseNumber++, 2, 10, new int[] { 2, 3, 5, 7 });
+            TestReturnValues(testCaseNumber++, 30, 48, new int[] { 31, 37, 41, 43, 47 });
+            TestReturnValues(testCaseNumber++, 11, 97, new int[] { 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 });
+            TestReturnValues(testCaseNumber++, 180, 190, new int[] { 181 });
+            TestException<ArgumentException>(testCaseNumber++, -1, -1);
+            TestException<ArgumentException>(testCaseNumber++, -1, 100);
+            TestException<ArgumentException>(testCaseNumber++, 0, 0);
+            TestException<ArgumentException>(testCaseNumber++, 0, 100);
+            TestException<ArgumentException>(testCaseNumber++, 1, 1);//
+            TestException<ArgumentException>(testCaseNumber++, 1, 100);//
+            TestException<ArgumentException>(testCaseNumber++, 2, -1);
+            TestException<ArgumentException>(testCaseNumber++, 2, 0);
+            TestException<ArgumentException>(testCaseNumber++, 2, 1);
+
+            if (correctTestCaseAmount == testCaseNumber - 1)
+            {
+                Console.WriteLine("Task is done.");
+            }
+            else
+            {
+                Console.WriteLine("TASK IS NOT DONE.");
+            }
+        }
+
+        private static void TestReturnValues(int testCaseNumber, int start, int end, int[] expectedResult)
+        {
+            int[] primeNumbers;
+            try
+            {
+                primeNumbers = GetPrimeNumbers(start, end);
+                if (primeNumbers.SequenceEqual(expectedResult))
+                {
+                    Console.WriteLine(correctCaseTemplate, testCaseNumber);
+                    correctTestCaseAmount++;
+                }
+                else
+                {
+                    Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+            }
+        }
+
+        private static void TestException<T>(int testCaseNumber, int startValue, int endValue) where T : Exception
+        {
+            try
+            {
+                GetPrimeNumbers(startValue, endValue);
+                Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+            }
+            catch (T)
+            {
+                Console.WriteLine(correctCaseTemplate, testCaseNumber);
+                correctTestCaseAmount++;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("#{0} - case is not correct!", testCaseNumber);
+            }
+        }
+
+        private static string correctCaseTemplate = "Case #{0} is correct.";
+        private static string incorrectCaseTemplate = "Case #{0} IS NOT CORRECT";
+        private static int correctTestCaseAmount = 0;
+    } 
+}
